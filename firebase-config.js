4d92d3fc99656dcb1783154e5ee2925a50650dde
mottanetdocumentacao-max <mottanetdocumentacao@gmail.com>
2026-06-27 11:48:34 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyXXXXXX...",
-  authDomain: "seu-projeto.firebaseapp.com",
-  databaseURL: "https://seu-projeto-default-rtdb.firebaseio.com",
-  projectId: "seu-projeto",
-  storageBucket: "seu-projeto.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abc123"
+  apiKey: "AIzaSyCeTMgCAIIFabiULj04PhFeX5TFQYN1rPY",
+  authDomain: "conferencia-provedor.firebaseapp.com",
+  databaseURL: "https://conferencia-provedor-default-rtdb.firebaseio.com",
+  projectId: "conferencia-provedor",
+  storageBucket: "conferencia-provedor.firebasestorage.app",
+  messagingSenderId: "807276649039",
+  appId: "1:807276649039:web:dc8fee0577c5027d03a83a"
 };
